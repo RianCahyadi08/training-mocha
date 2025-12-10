fee62a8747fe4f3e713e371f2d30407aa3432b98
LAPTOP-RRMTRDPL\Rian Cahyadi <ryancahyadi11@gmail.com>
2025-12-10 23:22:36 +0700

    improvement

--- src/tests/Login.js
+++ src/tests/Login.js
@@ -1,4 +1,4 @@
-import { expect } from "@wdio/globals";
+import { driver, expect } from "@wdio/globals";
 import LoginPage from "../pages/LoginPage";
 import OnboardingPage from "../pages/OnboardingPage";
 import HomePage from "../pages/HomePage";
@@ -6,9 +6,14 @@ import HomePage from "../pages/HomePage";
 describe("User on login page", () => {
   it("User login successfully", async () => {
     await OnboardingPage.tapSkip();
+    await driver.pause(2000);
     await LoginPage.tapEmailTab();
+    await driver.pause(2000);
     await LoginPage.loginEmail("rian.cahyadi@mydigilearn.id", "P@55word");
+    await driver.pause(2000);
     await HomePage.homeMenu.waitForDisplayed({ timeout: 5000 });
+    await driver.pause(2000);
     await expect(HomePage.homeMenu).toBeDisplayed();
+    await driver.pause(2000);
   });
 });
