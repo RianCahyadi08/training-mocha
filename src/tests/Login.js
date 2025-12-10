a8563170e730d5bdd2247cb8f6e4cc3a37e2bf3b
LAPTOP-RRMTRDPL\Rian Cahyadi <ryancahyadi11@gmail.com>
2025-12-10 23:07:23 +0700

    add report allure

--- src/tests/Login.js
+++ src/tests/Login.js
@@ -6,8 +6,11 @@ import HomePage from "../pages/HomePage";
 describe("User on login page", () => {
   it("User login successfully", async () => {
     await OnboardingPage.tapSkip();
+    await driver.pause(1000);
     await LoginPage.tapEmailTab();
+    await driver.pause(1000);
     await LoginPage.loginEmail("rian.cahyadi@mydigilearn.id", "P@55word");
+    await driver.pause(1000);
     await HomePage.homeMenu.waitForDisplayed({ timeout: 5000 });
     await expect(HomePage.homeMenu).toBeDisplayed();
   });
