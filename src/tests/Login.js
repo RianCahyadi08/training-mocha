5329fc68ecbaeb800d4dc0ba95151228aad04987
LAPTOP-RRMTRDPL\Rian Cahyadi <ryancahyadi11@gmail.com>
2025-12-11 21:44:04 +0700

    integrasi report test allure

--- src/tests/Login.js
+++ src/tests/Login.js
@@ -6,14 +6,13 @@ import HomePage from "../pages/HomePage";
 describe("User on login page", () => {
   it("User login successfully", async () => {
     await OnboardingPage.tapSkip();
-    await driver.pause(2000);
+    await driver.pause(500);
     await LoginPage.tapEmailTab();
-    await driver.pause(2000);
+    await driver.pause(500);
     await LoginPage.loginEmail("rian.cahyadi@mydigilearn.id", "P@55word");
-    await driver.pause(2000);
+    await driver.pause(500);
     await HomePage.homeMenu.waitForDisplayed({ timeout: 5000 });
-    await driver.pause(2000);
+    await driver.pause(500);
     await expect(HomePage.homeMenu).toBeDisplayed();
-    await driver.pause(2000);
   });
 });
